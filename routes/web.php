38946a5f151ec8f9b7464c3189dcfb59cb716ebb
Samuele <samueletasciotti@icloud.com>
2024-09-16 16:00:00 +0200

    feat: ComicController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-   
-    return view('home', compact('text', 'title'));
-})->name('home');
+Route::get('/',[ComicController::class, 'index'])->name('home');
 
+
+Route::resource('comics', ComicController::class);
